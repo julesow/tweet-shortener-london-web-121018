78eb6e5651affc2ba36894929ffffc8e2a64c49b
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-12-05 09:22:36 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,19 @@
+require "pry"
 # Write your code here.
+dictionary = {
+    "hello" =>'hi',
+"to, two, too" => '2',
+"for, four"=> '4',
+'be' => 'b',
+'you'=>'u',
+"at" => "@" ,
+"and" => "&"
+  }
+  
+  def word_substituter(tweet)
+    binding.pry
+   puts tweet.split("")
+   end
+    
+  end
+  
