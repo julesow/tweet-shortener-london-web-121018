6988ddfff580ef76bd2fdd595e6cc414f4cdc261
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-12-05 10:39:20 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,27 @@
+require "pry"
 # Write your code here.
+def dictionary {
+    "hello" =>'hi',
+"to, two, too" => '2',
+"for, four"=> '4',
+'be' => 'b',
+'you'=>'u',
+"at" => "@" ,
+"and" => "&"
+  }
+ end  
+  def word_substituter(tweet)
+        
+       words = tweet.split("")
+       words.each do |word|
+         dictionary.each do |k,v|
+          if word.include?(dictionary.keys)
+      
+           words << word.replace(dictionary[v])
+         end
+       end
+      puts tweet = words.join(" ")
+   end
+    
+  end
+  
